9cd03bdba6c408c17e0dadc4575daf94f6169650
user <user@computer>
2018-02-04 23:28:00 +0300

    code cleanups and readme

--- xml_handler.py
+++ xml_handler.py
@@ -8,10 +8,8 @@ Constants:
     OBJECT_NAME_LENGTH (int): The length of the object names generated.
     RANDOM_SEED (int): Random seed for results reproductivity.
     DATA_DIR (str): The working directory for zip files.
-
-Todo:
-    * Unit-tests
-    * Error handling
+    NUM_ZIP (int): The number of zip files to create.
+    NUM_XML (int): The number of xml files per each zip file.
 """
 
 import multiprocessing as mp
@@ -25,6 +23,8 @@ UID_LENGTH = 16
 OBJECT_NAME_LENGTH = 16
 RANDOM_SEED = 42
 DATA_DIR = './'
+NUM_ZIP = 50
+NUM_XML = 100
 
 
 def main():
@@ -32,7 +32,7 @@ def main():
     See README.md for more info.
     """
     random.seed(RANDOM_SEED)
-    stage1(DATA_DIR)
+    stage1(DATA_DIR, NUM_ZIP, NUM_XML)
     stage2(DATA_DIR)
 
 
@@ -44,25 +44,23 @@ def stage1(path='./', num_zip=50, num_xml=100):
         path (str): Working directory.
         num_zip (int): Number of zip files to create.
         num_xml (int): Number of xml in each zip file.
-
-    Returns:
-        bool: The return value. True for success, False otherwise.
     """
     uids = set()
-    pool = mp.Pool()
-    results = []
 
     while len(uids) < num_zip * num_xml:
         uids.add(make_random_string(UID_LENGTH))
     
     fname_len = len(str(max(num_zip, num_xml)))
 
+    pool = mp.Pool()
+    results = []
+
     for i in range(num_zip):
         results.append(pool.apply_async(make_zip, 
             args=(path + str(i + 1).zfill(fname_len) + '.zip', [uids.pop() for _ in range(num_xml)], fname_len)))
     
-    results = [p.get() for p in results]
-        
+    results = [r.get() for r in results]
+
 
 def make_zip(file_name, uids, fname_len):
     """
@@ -73,6 +71,7 @@ def make_zip(file_name, uids, fname_len):
         uids (list of str): List of uids.
         fname_len (int): Length of name for xml files (w/o ext).
     """
+    # TODO: check if we can write to the directory
     with zipfile.ZipFile(file_name, 'w') as fzip:
         for i in range(len(uids)):
             fzip.writestr(str(i + 1).zfill(fname_len) + '.xml', make_xml(uids[i]))
@@ -146,10 +145,11 @@ def stage2(path='./'):
     pool = mp.Pool()
     results = []
 
-    for file in os.listdir(path):
-        if file.endswith('.zip'):
-            results.append(pool.apply_async(parse_zip, args=(path + file, regexes)))
+    for file_name in os.listdir(path):
+        if file_name.endswith('.zip'):
+            results.append(pool.apply_async(parse_zip, args=(path + file_name, regexes)))
 
+    # TODO: check if we can write to the directory
     with open('1.csv', 'w') as fcsv:
         fcsv.write(''.join(['id, level\n'] + [r.get()[0] for r in results]))
 
@@ -169,15 +169,19 @@ def parse_zip(file_name, regexes):
         list of str: 2 blocks of csv data.
     """
     csv1, csv2 = [], []
-    zfile = zipfile.ZipFile(file_name, 'r')
+    fzip = zipfile.ZipFile(file_name, 'r')
+    # TODO: check if the zip file exists
 
-    for name in zfile.namelist():
-        if name.endswith('.xml'):
-            xml = str(zfile.read(name))
-            uid = regexes[0].findall(xml)[0]
-            csv1.append('{}, {}\n'.format(uid, regexes[1].findall(xml)[0]))
-            csv2.append(uid + ', ' + ('\n' + uid + ', ').join(regexes[2].findall(xml)) + '\n')
-    
+    for file_name in fzip.namelist():
+        if file_name.endswith('.xml'):
+            xml = str(fzip.read(file_name))
+            # TODO: check if xml has correct structure
+            try:
+                uid = regexes[0].findall(xml)[0]
+                csv1.append('{}, {}\n'.format(uid, regexes[1].findall(xml)[0]))
+                csv2.append(uid + ', ' + ('\n' + uid + ', ').join(regexes[2].findall(xml)) + '\n')
+            except:
+                pass
     return ''.join(csv1), ''.join(csv2)
 
 
